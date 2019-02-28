f0908be42d63755254d4135b723d1bc2cd67a45e
AleksApla <f4van@yandex.ru>
2019-02-28 15:47:17 +0300

    Изменил название класса

--- src/com/company/Array.java
+++ src/com/company/Array.java
@@ -6,19 +6,19 @@ import java.util.Arrays;
 import java.util.Scanner;
 import java.text.DecimalFormat;
 
-public class Binar {
+public class Array {
 
     public static void main(String[] args){
 
         Scanner a = new Scanner(System.in);
-        System.out.print("Введите число ");
+        System.out.println("Введите число ");
         int curs = a.nextInt();
         int arr[] = new int[curs];
         for(int i = 0; i < arr.length; i++) {
             arr[i] = (int)(Math.random()*100);
             System.out.print(arr[i] + " ");
         }
-        System.out.print("\nSorted: \n");
+        System.out.println("\nСортировка: ");
         Arrays.sort(arr);
         for(int i = 0; i <  arr.length; i++) {
             System.out.print(arr[i] + "  ");
